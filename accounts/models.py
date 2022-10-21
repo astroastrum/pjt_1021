1006cdc930a573308a016376ac590a1e79508851
JungAh Choi <jungahchoi92@gmail.com>
2022-10-21 10:14:22 +0900

    accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    def full_name(self):
+        return f"{self.last_name}{self.first_name}"
