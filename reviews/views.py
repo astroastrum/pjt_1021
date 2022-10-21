cad615970544c2a4d232b87d017e7ee21e9140ec
cksthf3211 <cksthf321@gmail.com>
2022-10-21 11:37:46 +0900

    오류잡기

--- reviews/views.py
+++ reviews/views.py
@@ -4,8 +4,4 @@ from .models import Review, Comment
 # Create your views here.
 
 def index(request):
-    review = Review.objects.order_by('-pk')
-    context = {
-        'review': review
-    }
-    return render(request, 'reviews/index.html', context)
+    return render(request, 'reviews/index.html')
